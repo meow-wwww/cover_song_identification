e082be77630f44817b5bcd5ce666c15bf28786e7
yabukota <1800017830@pku.edu.cn>
2021-08-12 12:16:58 +0800

    增加两种损失函数：交叉熵+平方损失，分为简单的和T/F的

--- loss_function.py
+++ loss_function.py
@@ -92,9 +92,9 @@ class CrossEntropyLoss_for_FA_CE(nn.Module):
 # In[ ]:
 
 
-class CrossEntropyLoss_for_FA_SQ(nn.Module):
+class CrossEntropyLoss_for_FA_CESQ(nn.Module):
     def __init__(self):
-        super(CrossEntropyLoss_for_FA_CE, self).__init__()
+        super(CrossEntropyLoss_for_FA_CESQ, self).__init__()
         return
     def forward(self, output, one_hot):
         '''
@@ -104,25 +104,52 @@ class CrossEntropyLoss_for_FA_SQ(nn.Module):
                 shape: [N, f, t]
         [OUTPUT]是一个batch中不同样本的均值
         '''
+        
         output = output.squeeze(dim=1) # output: [N, f, t]
         sm = output + 1e-20
         
-        none_zero_lines_num = one_hot.bool().any(1).sum()
+        ce_loss = (-torch.log(sm)*one_hot).sum(dim=1)
+        sq_loss = (sm**2).sum(dim=1)
         
-        if none_zero_lines_num == 0:
-            return (-torch.log(sm)*one_hot).sum()*0
-        else:
-            return (-torch.log(sm)*one_hot).sum()/none_zero_lines_num
+        non_zero = one_hot.bool().any(1).to(torch.int32)
+        loss_all = ((1-non_zero)*sq_loss).sum()+ce_loss.sum()
         
-        # 对one-hot进行处理，没有label的全零列改成1/360
+        return loss_all/(output.shape[0]*output.shape[2])
 
-        none_zero_lines = one_hot.bool().any(1).reshape(-1)
-        one_hot = one_hot.float()
-        index0 = torch.tensor([list(range(one_hot.shape[0]))]*one_hot.shape[2]).T.reshape(-1)[none_zero_lines == False]
-        index2 = torch.tensor([list(range(one_hot.shape[2]))]*one_hot.shape[0]).reshape(-1)[none_zero_lines == False]
-        one_hot[index0, :, index2] = 1/one_hot.shape[1]
+
+# In[ ]:
+
+
+class CrossEntropyLoss_for_FA_CESQ_TF(nn.Module):
+    def __init__(self):
+        super(CrossEntropyLoss_for_FA_CESQ_TF, self).__init__()
+        return
+    def forward(self, output, one_hot):
+        '''
+        output: network output(not softmax yet)
+                shape: [N, 1, f, t]
+        one_hot: ground truth(has not been gaussian blurred)
+                shape: [N, f, t]
+        [OUTPUT]是一个batch中不同样本的均值
+        '''
         
-        return (-torch.log(sm)*one_hot).sum()/(output.shape[2]*output.shape[0])
+        output = output.squeeze(dim=1) # output: [N, f, t]
+        sm = output + 1e-20
+        
+        ce_loss = (-torch.log(sm)*one_hot).sum(dim=1)
+        sq_loss = (sm**2).sum(dim=1)
+        non_zero = one_hot.bool().any(1).to(torch.int32)
+        loss_all = ((1-non_zero)*sq_loss) + ce_loss
+        
+        Xout = utils.salience_to_output(output.clone().detach(), threshold=0.05).to(torch.int32)
+        cmp_time = (Xout != one_hot).to(torch.int32).sum(dim=1).bool().to(torch.int32) # 判断错误的列
+
+        False_loss = (loss_all * cmp_time).sum()
+        True_loss = loss_all.sum() - False_loss
+        False_num = cmp_time.sum()
+        True_num = output.shape[2]*output.shape[0] - False_num
+        
+        return False_loss, False_num, True_loss, True_num
 
 
 # In[8]:
@@ -203,8 +230,8 @@ class CrossEntropyLoss_for_FA_CE_TF(nn.Module):
         loss_time = (-torch.log(sm)*one_hot_float).sum(dim=1) # scalar tensor
         
         False_loss = (loss_time * cmp_time).sum()
-        True_loss = loss_time.sum() - False_loss
         False_num = cmp_time.sum()
+        True_loss = loss_time.sum() - False_loss
         True_num = num_all - False_num
         
         # ----------------------------------------------------------------------
